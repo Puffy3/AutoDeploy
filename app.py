88ee24dd4f145cd5593968b01948dc125cc93b69
prasun <prasun.1928@gmail.com>
2025-07-30 02:01:57 +0530

    modify app file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is working, right??....rigth??.....RIGHT?????'
+    return 'Finnaly... it works!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
