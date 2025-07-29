d4d462eccbbf51a0bc1d7ef43bf2adeaad996ff2
prasun <prasun.1928@gmail.com>
2025-07-30 01:31:22 +0530

    modify app file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Minikube! CI/CD is working.'
+    return 'Thios is working, right??....rigth??.....RIGHT?????'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
